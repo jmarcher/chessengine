4b343857c625efe0da078c4eb6247119f5254934
Joaquín Marcher <joaquin.marcher@outrightvision.com>
2022-05-17 11:12:21 +0200

    Fix typo

--- library.cpp
+++ library.cpp
@@ -5,5 +5,5 @@
 #include "models/Piece.h"
 
 void hello() {
-    std::cout << (Chess::Piece::isRookOrQueen(Chess::Piece::BLACK | Chess::Piece::QUEEN)) ? "Is a Queen" : "Not a Queen" << std::endl;
+    std::cout << (Chess::Piece::isRookOrQueen(Chess::Piece::BLACK | Chess::Piece::QUEEN) ? "Is a Queen" : "Not a Queen") << std::endl;
 }
