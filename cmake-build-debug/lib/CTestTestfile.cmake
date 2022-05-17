9d7e54eca499c8f2b017086461b06cf38ee9131c
Joaquín Marcher <joaquin.marcher@outrightvision.com>
2022-05-17 10:47:46 +0200

    Delete files

--- cmake-build-debug/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gordo/CLionProjects/chessengine/lib
-# Build directory: /home/gordo/CLionProjects/chessengine/cmake-build-debug/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gordo/CLionProjects/chessengine/tests/lib/googlemock
-# Build directory: /home/gordo/CLionProjects/chessengine/cmake-build-debug/tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
